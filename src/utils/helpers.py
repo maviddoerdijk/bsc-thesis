afccae51a5a076cf1ef2a87460f384059f5dd529
david <davidmoerdijk@gmail.com>
2025-05-24 10:51:18 +0200

    add util function for calculating train, dev and test fractions

---
+++ src/utils/helpers.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+def _get_train_dev_frac(startDateStr, endDateStr, startDateStrTest, endDateStrTest, verbose=False):
+  """
+  For certain periods, we want a specific testing period, and must therefore calculate the train/dev split based on that.
+  """
+  # convert all 4 dates to datetime
+  startDateStr = datetime.strptime(startDateStr, '%Y-%m-%d')
+  endDateStr = datetime.strptime(endDateStr, '%Y-%m-%d')
+  startDateStrTest = datetime.strptime(startDateStrTest, '%Y-%m-%d')
+  endDateStrTest = datetime.strptime(endDateStrTest, '%Y-%m-%d')
+
+  total_days = (endDateStr - startDateStr).days
+  test_days = (endDateStrTest - startDateStrTest).days
+  train_days = total_days - 2 * test_days
+
+  train_frac = train_days / total_days
+  test_frac = test_days / total_days
+  dev_frac = test_frac
+
+  if verbose:
+    print(f"train_frac: {train_frac}")
+    print(f"dev_frac: {dev_frac}")
+    print(f"test_frac: {test_frac}")
+
+  return train_frac, dev_frac
